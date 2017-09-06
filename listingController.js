04296863459de54cb2dbc88890d519e00e04dc66
Allison Kast <allisonakast@gmail.com>
2017-09-06 17:08:20 -0400

    add more style

--- listingController.js
+++ listingController.js
@@ -7,8 +7,34 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+
+    $scope.addListing = function() {
+        if ($scope.listing.code && $scope.listing.name){
+          $scope.listing.code = $scope.listing.code.toUpperCase();
+          $scope.listings.push($scope.listing);
+          $scope.listing = "";
+        }
+    };
+
+    $scope.deleteListing = function(index) {
+        for(i in $scope.listings) {
+          if($scope.listings[i].code == index.code) {
+            $scope.listings.splice(i, 1);
+          }
+        }
+    };
+
+    $scope.showDetails = function(index) {
+        $scope.detailedInfo = index;
+        $scope.detailedInfo.coordinate
+        if (!$scope.detailedInfo.code) $scope.detailedInfo.code = "N/A";
+        if (!$scope.detailedInfo.name) $scope.detailedInfo.name = "N/A";
+        if (!$scope.detailedInfo.coordinates) $scope.detailedInfo.coordinates = "N/A";
+        if (!$scope.detailedInfo.address) $scope.detailedInfo.address = "N/A";
+    };
+
+    $scope.clearDetails = function(){
+        $scope.detailedInfo = "";
+    }
   }
 ]);
