195a81a5db8db43720f7b4a0f112882678b30091
Allison Kast <allisonakast@gmail.com>
2017-09-06 20:17:48 -0400

    assignment1 upload

--- server.js
+++ server.js
@@ -7,20 +7,27 @@ var http = require('http'),
 var listingData, server;
 
 var requestHandler = function(request, response) {
-  var parsedUrl = url.parse(request.url);
-
-  /*
-    Your request handler should send listingData in the JSON format if a GET request 
-    is sent to the '/listings' path. Otherwise, it should send a 404 error. 
-
-    HINT: explore the request object and its properties 
-    http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
-   */
+  var parsedUrl = url.parse(request.url).pathname;
+  
+  if (parsedUrl == '/listings'){
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.write(listingData);
+        response.end();
+  }
+  else{
+        response.writeHead(404, {'Content-Type': 'text/plain'});
+        response.write('Bad gateway error'),
+        response.end();
+  }
 };
 
 fs.readFile('listings.json', 'utf8', function(err, data) {
-  /*
-    This callback function should save the data in the listingData variable, 
-    then start the server. 
-   */
+  if (err) throw err
+  listingData = JSON.stringify(JSON.parse(data));
+
+  server.listen(port, function() {
+    console.log('Server listening on ' + port);
+  });
 });
+
+server = http.createServer(requestHandler);
