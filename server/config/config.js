dfce4ba8a5a1674e24a3d9c2cda628b9401d4ce1
Allison Kast <allisonakast@gmail.com>
2017-09-22 14:31:42 -0400

    Merge branch 'Assignment5' unignore config
    update
    
    update
    
    package
    
    procfile
    
    https
    
    https
    
    try it

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    db: {
+      uri: 'mongodb://admin:216604@ds115124.mlab.com:15124/akast', //place the URI of your mongo database here.
+    }, 
+    googleMaps: {
+      key: 'AIzaSyC-MOjsXQ9b45Ib5vY860-gBgxyuRoCGUQ'
+    },
+    port: process.env.PORT
+  };
